d553f6feacf612ad9e66083cd8953d977f33fe21
Wilson <willdev61@gmail.com>
2023-01-02 18:34:53 +0100

    remove defaultAminModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,8 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import DefaultAdminModule from 'nestjs-admin/dist/src/defaultAdmin.module';
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), DefaultAdminModule],
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersService],
   controllers: [UsersController],
 })
